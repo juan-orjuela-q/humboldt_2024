97360aa77b43aaa113091345155dce774eddb873
Juan Santil <juan.santil@appicua.com>
2025-10-24 18:27:32 -0400

    fix merge conflict

--- contenido/ficha_106/bio2024-106-scripts.js
+++ contenido/ficha_106/bio2024-106-scripts.js
@@ -319,120 +319,121 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const container = document.getElementById("barranquilla-graphic");
 if (container) {
-Highcharts.chart("barranquilla-graphic", {
+  Highcharts.chart("barranquilla-graphic", {
     chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        height: 600,
-        type: "pie",
-        style: {
-            fontFamily: "Rubik, sans-serif",
-        },
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      height: 600,
+      type: "pie",
+      style: {
+        fontFamily: "Rubik, sans-serif",
+      },
     },
     title: {
-        text: "Expedición científica sobre la biodiversidad funcional en agroecosistemas",
-        style: {
-            color: "#000",
-            fontFamily: "Rubik, sans-serif",
-            fontWeight: "bold",
-        },
+      text: "Expedición científica sobre la biodiversidad funcional en agroecosistemas",
+      style: {
+        color: "#000",
+        fontFamily: "Rubik, sans-serif",
+        fontWeight: "bold",
+      },
     },
     tooltip: {
-        backgroundColor: "#FFFFFF",
-        borderColor: "#CCCCCC",
-        borderRadius: 8,
-        borderWidth: 1,
-        pointFormat: '<span style="color:{point.color}">●</span> {point.name}<br/>' +
-                     'Cantidad: <b>{point.y}</b><br/>' +
-                     'Porcentaje: <b>{point.percentage:.1f}%</b>',
-        style: {
-            color: "#000",
-            fontFamily: "Rubik, sans-serif",
-        },
+      backgroundColor: "#FFFFFF",
+      borderColor: "#CCCCCC",
+      borderRadius: 8,
+      borderWidth: 1,
+      pointFormat:
+        '<span style="color:{point.color}">●</span> {point.name}<br/>' +
+        "Cantidad: <b>{point.y}</b><br/>" +
+        "Porcentaje: <b>{point.percentage:.1f}%</b>",
+      style: {
+        color: "#000",
+        fontFamily: "Rubik, sans-serif",
+      },
     },
     accessibility: {
-        point: {
-            valueSuffix: "%",
-        },
+      point: {
+        valueSuffix: "%",
+      },
     },
     legend: {
-        align: "right",
-        verticalAlign: "middle",
-        layout: "vertical",
-        itemStyle: {
-            color: "#000",
-            fontFamily: "Rubik, sans-serif",
-            fontSize: "12px",
-        },
-        itemHoverStyle: {
-            color: "#333",
-        },
+      align: "right",
+      verticalAlign: "middle",
+      layout: "vertical",
+      itemStyle: {
+        color: "#000",
+        fontFamily: "Rubik, sans-serif",
+        fontSize: "12px",
+      },
+      itemHoverStyle: {
+        color: "#333",
+      },
     },
     plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: "pointer",
-            dataLabels: {
-                enabled: false,
-            },
-            showInLegend: true,
+      pie: {
+        allowPointSelect: true,
+        cursor: "pointer",
+        dataLabels: {
+          enabled: false,
         },
+        showInLegend: true,
+      },
     },
     series: [
-        {
-            name: "Categorías",
-            colorByPoint: true,
-            data: [
-                {
-                    name: "Medicinal",
-                    y: 1080,
-                    sliced: false,
-                    selected: true,
-                    color: "#3F80EA",
-                },
-                {
-                    name: "Alimentos",
-                    y: 585,
-                    color: "#E14033",
-                },
-                {
-                    name: "Materiales",
-                    y: 412,
-                    color: "#F1B504",
-                },
-                {
-                    name: "Usos ambientales",
-                    y: 279,
-                    color: "#32A150",
-                },
-                {
-                    name: "Alimento de animales",
-                    y: 170,
-                    color: "#F56901",
-                },
-                {
-                    name: "Cosméticos",
-                    y: 157,
-                    color: "#43B6BE",
-                },
-                {
-                    name: "Sociales (incluye espiritual y religioso)",
-                    y: 110,
-                    color: "#76A3ED",
-                },
-                {
-                    name: "Veneno o tóxico",
-                    y: 66,
-                    color: "#E8796E",
-                },
-                {
-                    name: "Leña o combustible",
-                    y: 50,
-                    color: "#F2C84C",
-                },
-            ],
-        },
+      {
+        name: "Categorías",
+        colorByPoint: true,
+        data: [
+          {
+            name: "Medicinal",
+            y: 1080,
+            sliced: false,
+            selected: true,
+            color: "#3F80EA",
+          },
+          {
+            name: "Alimentos",
+            y: 585,
+            color: "#E14033",
+          },
+          {
+            name: "Materiales",
+            y: 412,
+            color: "#F1B504",
+          },
+          {
+            name: "Usos ambientales",
+            y: 279,
+            color: "#32A150",
+          },
+          {
+            name: "Alimento de animales",
+            y: 170,
+            color: "#F56901",
+          },
+          {
+            name: "Cosméticos",
+            y: 157,
+            color: "#43B6BE",
+          },
+          {
+            name: "Sociales (incluye espiritual y religioso)",
+            y: 110,
+            color: "#76A3ED",
+          },
+          {
+            name: "Veneno o tóxico",
+            y: 66,
+            color: "#E8796E",
+          },
+          {
+            name: "Leña o combustible",
+            y: 50,
+            color: "#F2C84C",
+          },
+        ],
+      },
       {
         name: "Categorías",
         colorByPoint: true,
@@ -487,7 +488,7 @@ Highcharts.chart("barranquilla-graphic", {
         ],
       },
     ],
-});
+  });
 
   Highcharts.chart("montes-graphic", {
     chart: {
@@ -509,17 +510,18 @@ Highcharts.chart("barranquilla-graphic", {
       },
     },
     tooltip: {
-        backgroundColor: "#FFFFFF",
-        borderColor: "#CCCCCC",
-        borderRadius: 8,
-        borderWidth: 1,
-        pointFormat: '<span style="color:{point.color}">●</span> {point.name}<br/>' +
-                     'Cantidad: <b>{point.y}</b><br/>' +
-                     'Porcentaje: <b>{point.percentage:.1f}%</b>',
-        style: {
-            color: "#000",
-            fontFamily: "Rubik, sans-serif",
-        },
+      backgroundColor: "#FFFFFF",
+      borderColor: "#CCCCCC",
+      borderRadius: 8,
+      borderWidth: 1,
+      pointFormat:
+        '<span style="color:{point.color}">●</span> {point.name}<br/>' +
+        "Cantidad: <b>{point.y}</b><br/>" +
+        "Porcentaje: <b>{point.percentage:.1f}%</b>",
+      style: {
+        color: "#000",
+        fontFamily: "Rubik, sans-serif",
+      },
       backgroundColor: "#FFFFFF",
       borderColor: "#CCCCCC",
       borderRadius: 8,
